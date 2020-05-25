ed5ac6c132248e0a4128f3865508d38c96921bf6
Nicky Logan <laurentiusdominick@yahoo.com>
2020-05-25 17:03:14 +0700

    build: move cra files to renderer

--- scripts/renderer/build.js
+++ scripts/renderer/build.js
@@ -12,15 +12,15 @@ process.on('unhandledRejection', err => {
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require('../../config/renderer/env');
 
 
 const path = require('path');
 const chalk = require('react-dev-utils/chalk');
 const fs = require('fs-extra');
 const webpack = require('webpack');
-const configFactory = require('../config/webpack.config');
-const paths = require('../config/paths');
+const configFactory = require('../../config/renderer/webpack.config');
+const paths = require('../../config/renderer/paths');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
--- scripts/renderer/start.js
+++ scripts/renderer/start.js
@@ -12,7 +12,7 @@ process.on('unhandledRejection', err => {
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require('../../config/renderer/env');
 
 
 const fs = require('fs');
@@ -28,9 +28,9 @@ const {
   prepareUrls,
 } = require('react-dev-utils/WebpackDevServerUtils');
 const openBrowser = require('react-dev-utils/openBrowser');
-const paths = require('../config/paths');
-const configFactory = require('../config/webpack.config');
-const createDevServerConfig = require('../config/webpackDevServer.config');
+const paths = require('../../config/renderer/paths');
+const configFactory = require('../../config/renderer/webpack.config');
+const createDevServerConfig = require('../../config/renderer/webpackDevServer.config');
 
 const useYarn = fs.existsSync(paths.yarnLockFile);
 const isInteractive = process.stdout.isTTY;
