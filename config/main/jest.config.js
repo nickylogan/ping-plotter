0e52ab453e4077c565923a757a860639b5c88fcd
Nicky Logan <laurentiusdominick@yahoo.com>
2020-08-23 01:38:34 +0700

    test(main): add test for ping implementation

---
+++ config/main/jest.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  rootDir: './../../',
+  roots: [
+    '<rootDir>/src/main'
+  ],
+  collectCoverageFrom: [
+    'src/main/**/*.{js,jsx,ts,tsx}',
+    '!src/main/**/*.d.ts'
+  ],
+  testMatch: [
+    '<rootDir>/src/main/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/main/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$'
+  ],
+  modulePaths: [],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy'
+  },
+  moduleFileExtensions: [
+    'web.js',
+    'js',
+    'web.ts',
+    'ts',
+    'web.tsx',
+    'tsx',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node'
+  ],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ]
+};
